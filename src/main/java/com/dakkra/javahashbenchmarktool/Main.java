038749e1dec55a7a5d1418d45e584fdf41930ef1
Dakkra <contact@dakkra.com>
2022-06-07 01:09:12 -0600

    initialize repository

---
+++ src/main/java/com/dakkra/javahashbenchmarktool/Main.java
@@ -0,0 +1,12 @@
+package com.dakkra.javahashbenchmarktool;
+
+public class Main {
+
+	public static void main( String[] args ) {
+		System.out.println( "Hello world!" );
+	}
+
+	public static String dummyTest() {
+		return "dummy";
+	}
+}
---
+++ src/test/java/com/dakkra/javahashbenchmarktool/MainTest.java
@@ -0,0 +1,13 @@
+package com.dakkra.javahashbenchmarktool;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+	@Test
+	void dummyTest() {
+		assertEquals( "dummy",  Main.dummyTest());
+	}
+}
