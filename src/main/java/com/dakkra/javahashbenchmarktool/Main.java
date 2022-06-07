43b40ad0f4d816ddae1669f28f3c89d7457f146b
Dakkra <contact@dakkra.com>
2022-06-07 02:35:22 -0600

    Reformat main

--- src/main/java/com/dakkra/javahashbenchmarktool/Main.java
+++ src/main/java/com/dakkra/javahashbenchmarktool/Main.java
@@ -22,53 +22,53 @@ public class Main {
 
 		int size = 2048;
 		String data = generateRandomData( size );
-		System.out.println("Generated random data of size " + size);
+		System.out.println( "Generated random data of size " + size );
 
-		System.out.println("Doing single thread bench...");
+		System.out.println( "Doing single thread bench..." );
 		HashWork single = new HashWork( scores, data );
 		single.startWork();
 		single.joinThread();
-		System.out.println("Single thread score is: " + scores.get( 0 ));
+		System.out.println( "Single thread score is: " + scores.get( 0 ) );
 
 		scores.clear();
-		System.out.println("Now doing multi-thread test...");
-		ArrayList<HashWork> workers = new ArrayList<>(coreCount);
-		for (int i = 0; i < coreCount; i++){
+		System.out.println( "Now doing multi-thread test..." );
+		ArrayList<HashWork> workers = new ArrayList<>( coreCount );
+		for( int i = 0; i < coreCount; i++ ) {
 			workers.add( new HashWork( scores, data ) );
 		}
-		for (HashWork worker : workers) {
+		for( HashWork worker : workers ) {
 			worker.startWork();
 		}
-		for (HashWork worker : workers) {
+		for( HashWork worker : workers ) {
 			worker.joinThread();
 		}
 		long sum = 0;
-		for (Integer score : scores){
+		for( Integer score : scores ) {
 			sum += score;
 		}
-		double avg = (double) sum / coreCount;
-		System.out.println("Number of scores: " + scores.size());
-		System.out.println("Total score is: " + sum);
-		System.out.println("With avg per core: " + avg);
+		double avg = (double)sum / coreCount;
+		System.out.println( "Number of scores: " + scores.size() );
+		System.out.println( "Total score is: " + sum );
+		System.out.println( "With avg per core: " + avg );
 	}
 
 	/**
 	 * Generates random data and returns it as a string
+	 *
 	 * @param size number of bytes to generate, this must be greater than 0
 	 * @return String representing random data
-	 *
 	 * @throws IllegalArgumentException if size <= 0
 	 */
 	public static String generateRandomData( int size ) {
 
-		if (size <= 0) {
-			throw new IllegalArgumentException("");
+		if( size <= 0 ) {
+			throw new IllegalArgumentException( "" );
 		}
 
 		Random r = new Random();
-		byte[] data = new byte[size];
+		byte[] data = new byte[ size ];
 		r.nextBytes( data );
 
-		return new String(data);
+		return new String( data );
 	}
 }
